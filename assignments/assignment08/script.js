d2fbb05c945bf5efb39086db638be134ad251179
bdshilli <bdshilli@gmail.com>
2024-05-15 16:18:16 -0400

    assignment 8

--- assignments/assignment08/script.js
+++ assignments/assignment08/script.js
@@ -20,5 +20,45 @@ document.getElementById("toggle-e2").onclick = () => {
 //exercise one - color slider
 document.getElementById("colorSlider").oninput = () => {
     const val = document.getElementById("colorSlider").value;
-    alert(val);
+    const item = document.getElementById("exercise1");
+    const text = document.getElementById("color-text");
+    item.style.backgroundColor = "rgb("+ val + ",0,0)";
+    switch (true) {
+        case val > 250:
+            text.innerHTML = "RED HOT!";
+            break;
+        case val > 180:
+            text.innerHTML = "hotter";
+            break;
+        case val > 90:
+            text.innerHTML = "mild";
+            break;
+        case val <= 90:
+            text.innerHTML = "calm";
+            break;
+        default:
+            break;
+    }
 }
+
+//exercise two - Picture Chooser
+function selectPicture(picType) {
+    const pic = document.getElementById("picture");
+    switch (picType) {
+        case "s":
+            pic.src = "https://picsum.photos/100/100";
+            break;
+        case "m":
+            pic.src = "https://picsum.photos/200/200";
+            break;
+        case "l":
+            pic.src = "https://picsum.photos/300/300";
+            break;
+        default:
+            break;
+    }
+}
+
+document.getElementById("small").onclick = () => {selectPicture("s");}
+document.getElementById("medium").onclick = () => {selectPicture("m");}
+document.getElementById("large").onclick = () => {selectPicture("l");}

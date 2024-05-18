34d3b5e5e7c8446e25bd168964ca7e984c4144ac
bdshilli <bdshilli@gmail.com>
2024-05-17 21:41:03 -0400

    assignment 9

--- assignments/assignment09/script.js
+++ assignments/assignment09/script.js
@@ -16,12 +16,24 @@ const validNum = (num, errorId) =>{
 }
 
 //Loop Stars
-document.getElementById("btn-loops").onclick = () => {
-
+document.getElementById("btn-draw").onclick = () => {
     const numStars = document.getElementById("txt-num").value;
     console.log(numStars);
     if(!validNum(numStars, "error-num")){
         return;
+    }
+    const starDiv = document.getElementById("div-stars");
+    starDiv.innerHTML = "";
+    for(let i = 0; i < numStars ; i++) {
+        const star = document.createElement("div");
+        star.classList.add("star");
+        star.style.setProperty("left", ((Math.random() * 387)) + "px");
+        star.style.setProperty("bottom", (4 + (Math.random() * 386)) + "px");
+        starDiv.appendChild(star);
+        star.onclick = () => {
+            const special = document.getElementById("info")
+            special.innerHTML = `You clicked star ${i+1}`;
+        }
     }
 
 }

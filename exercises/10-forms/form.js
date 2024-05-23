290c974aec6f114c8715099a8882181879e77a34
bdshilli <bdshilli@gmail.com>
2024-05-23 11:51:10 -0400

    classwork

---
+++ exercises/10-forms/form.js
@@ -0,0 +1,25 @@
+getRadioValue = (radioName) => {
+  const radios = document.getElementsByName(radioName);
+
+  for (let i in radios) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
+  }
+  return "";
+};
+
+document.getElementById("form-raccoon").onsubmit = (e) => {
+  e.preventDefault(); //dont go to the action page
+  const form = e.target;
+  const racoonName = form.elements["raccoon-name"].value;
+  const demeanor = form.elements["demeanor"].valu;
+  e;
+  const termsChecked = form.elements["terms"].checked;
+  const termsText = "You agrred to the terms";
+  const radio = getRadioValue("size");
+  if (!termsChecked) {
+    termsText = "You did not agree to the terms";
+  }
+  console.log(`${racoonName} the raccoon is ${radio} and ${demeanor}`);
+};
